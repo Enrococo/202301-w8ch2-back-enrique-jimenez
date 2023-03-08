63dc43b6a0161bccbeeda651fdffe65d67036d29
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-08 21:39:51 +0100

    Add app change

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import log from './logger';
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Server ON');
+});
+
+app.listen(port, () => {
+  log.info(`Example app listening on port ${port}`);
+});
+
+export default app;
