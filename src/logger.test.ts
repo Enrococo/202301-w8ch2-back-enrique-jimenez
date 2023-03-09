552f1b4f9e5cbb6efe6abc2e88c371206fc3ff22
`Enrique` <`e.jimenezmoreno@gmail.com`>
2023-03-09 13:28:08 +0100

    Remove comments from logger test

--- src/logger.test.ts
+++ src/logger.test.ts
@@ -8,13 +8,13 @@ describe('Given a log function', () => {
   });
 
   test('should set the log level to the value specified in the environment variable', () => {
-    process.env.BUNYAN_LEVEL = 'debug'; // Set the environment variable to a known value
+    process.env.BUNYAN_LEVEL = 'debug';
     log.level((process.env.BUNYAN_LEVEL as LogLevel) ?? 'info');
-    expect(log.level()).toEqual(20); // Check that the log level was set to the correct value
+    expect(log.level()).toEqual(20);
   });
   test('should set the log level to the value specified in the environment variable', () => {
     delete process.env.BUNYAN_LEVEL;
     log.level(process.env.BUNYAN_LEVEL ?? 'info');
-    expect(log.level()).toEqual(30); // Check that the log level was set to the correct value
+    expect(log.level()).toEqual(30);
   });
 });
